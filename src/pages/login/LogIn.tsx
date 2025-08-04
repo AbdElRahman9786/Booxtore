f425d00a8993d8ed5882c05cec10fc7c9dedc612
unknown <abdelrahmanahmed7088@gmail.com>
2025-08-04 22:10:51 +0300

    hanel login responseve

--- src/pages/login/LogIn.tsx
+++ src/pages/login/LogIn.tsx
@@ -1,6 +1,6 @@
 import img from "../../assets/images/login.ebd58562113a46604e6a.png"
 import TextField from '@mui/material/TextField';
-const LogIn = () => {
+const LogIn:React.FC = () => {
  
 
   
@@ -8,8 +8,8 @@ const LogIn = () => {
   return (
 
     <>
-    <div className="flex justify-between items-center h-screen bg-[#f3faf7] rtl">
-        <div className="w-1/2 p-10 ml-5 py-20 bg-white shadow-lg rounded-lg text-right">
+    <div className="flex justify-between items-center min-h-screen bg-[#f3faf7]  max-md:flex-wrap rtl">
+        <div className="w-1/2 p-10  ml-5 max-md:ml-0 max-md:order-2 py-20 bg-white shadow-lg rounded-lg text-right max-md:w-full">
             <h1 className="mb-10 font-bold text-3xl">:تسجيل الدخول</h1>
             <form className="min-w-full text-right">
                 
@@ -37,8 +37,10 @@ const LogIn = () => {
             </form>
             <p className="mt-5 text-sm">ليس لديك حساب؟ <a href="#" className="text-[#facc15]">انشاء حساب</a></p>
         </div>
-        <div className="h-screen">
-            <img src={img} alt="Book Collection" className="logo" />
+        <div className="max-md:w-full h-fullmax-md:order-1  max-md:h-auto ">
+            <img src={img} alt="Book Collection"  className="min-w-full max-md:h-1/2" />
+            
+             
         </div>
 
 

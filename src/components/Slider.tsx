302739c8a393a8143fd3d58240fb10caabc11942
abdelrahman55 <abdelrahmanahmed7088@gmail.com>
2025-08-14 14:19:03 +0300

    commplete slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,16 @@
 
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import img from '../assets/images/VVKYgjkAcUFuq7Dj3KUPJHKmiBluprQbf61syspG.png'
 import 'swiper/css/bundle';
-
-
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import AddIcon from '@mui/icons-material/Add';
 
 
 export const Slider = () => {
+  
   return (
     <>
     <div className='flex p-2 bg-[#f3faf7] max-md:flex-col-reverse gap-2 max-md:items-center max-md:justify-center  mt-56 '>
@@ -16,7 +18,7 @@ export const Slider = () => {
         <div className='w-full max-w-full overflow-x-hidden '>
            <Swiper
             
-      spaceBetween={50}
+      spaceBetween={10}
       slidesPerView={3}
       
       
@@ -35,52 +37,123 @@ export const Slider = () => {
   }}
     >
       <SwiperSlide>
-        <div className=' rounded-md border-1 boreder-[#b2ddd0]'>
+        <div className=' rounded-lg border-1 boreder-[rgb(64 140 123)] overflow-hidden'>
           <div className='relative overflow-hidden'>
 
-        <img src={img} alt='this book img' className=' border-b-1 boreder-[#b2ddd0] rounded-md hover:scale-105 duration-150 overflow-hidden peer '/>
-        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>llllllllllll</div>
+        <img src={img} alt='this book img' className=' border-b-1 boreder-[#b2ddd0] rounded-lg hover:scale-105 duration-150 overflow-hidden peer '/>
+        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>
+          <div className='flex items-center justify-center gap-2'>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <AddCircleOutlineIcon />
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+<RemoveCircleOutlineIcon/>
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <VisibilityIcon/>
+            </div>
+          </div>
         </div>
-        
-          <h3 className='text-center'>عنوان الكتاب</h3>
-          <p className='text-center'>وصف الكتاب</p>
-          <button className='bg-blue-500 text-white px-4 py-2 rounded'>قراءة المزيد</button>
+        </div>
+        <div className='p-4 text-end'>
+          <h3>اسم الكتاب </h3>
+          <p>وصف الكتاب</p>
+          <p>سعر الكتاب</p>
+          <div className='flex items-center justify-between '>
+            <button className='bg-[#408c7b] text-white px-4 py-2 rounded'>قراءة المزيد</button>
+          <p>تقيم الكتاب</p>
+        </div>
+          </div>
         </div>
         </SwiperSlide>
       <SwiperSlide>  
-        <div className=' rounded-md border-1 boreder-[#b2ddd0]'>
+         <div className=' rounded-lg border-1 boreder-[rgb(64 140 123)] overflow-hidden'>
           <div className='relative overflow-hidden'>
 
-        <img src={img} alt='this book img' className=' border-b-1 boreder-[#b2ddd0] rounded-md  hover:scale-105 hover:rounded-md duration-150 overflow-hidden peer '/>
-        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>llllllllllll</div>
+        <img src={img} alt='this book img' className=' border-b-1 boreder-[#b2ddd0] rounded-lg hover:scale-105 duration-150 overflow-hidden peer '/>
+        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>
+          <div className='flex items-center justify-center gap-2'>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <AddCircleOutlineIcon />
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+<RemoveCircleOutlineIcon/>
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <VisibilityIcon/>
+            </div>
+          </div>
+        </div>
         </div>
         
-          <h3 className='text-center'>عنوان الكتاب</h3>
-          <p className='text-center'>وصف الكتاب</p>
-          <button className='bg-blue-500 text-white px-4 py-2 rounded'>قراءة المزيد</button>
+          <div className='p-4 text-end'>
+          <h3>اسم الكتاب </h3>
+          <p>وصف الكتاب</p>
+          <p>سعر الكتاب</p>
+          <div className='flex items-center justify-between '>
+            <button className='bg-[#408c7b] text-white px-4 py-2 rounded'>قراءة المزيد</button>
+          <p>تقيم الكتاب</p>
+        </div>
+          </div>
         </div>
         </SwiperSlide>
-      <SwiperSlide>     <div className=' rounded-md border-1 boreder-[#b2ddd0]'>
+      <SwiperSlide>      <div className=' rounded-lg border-1 boreder-[rgb(64 140 123)] overflow-hidden'>
           <div className='relative overflow-hidden'>
 
-        <img src={img} alt='this book img' className=' border-b-1 boreder-[#b2ddd0] rounded-md hover:scale-105 duration-150 overflow-hidden peer '/>
-        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>llllllllllll</div>
+        <img src={img} alt='this book img' className=' border-b-1 boreder-[#b2ddd0] rounded-lg hover:scale-105 duration-150 overflow-hidden peer '/>
+        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>
+          <div className='flex items-center justify-center gap-2'>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <AddCircleOutlineIcon />
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+<RemoveCircleOutlineIcon/>
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <VisibilityIcon/>
+            </div>
+          </div>
+        </div>
         </div>
         
-          <h3 className='text-center'>عنوان الكتاب</h3>
-          <p className='text-center'>وصف الكتاب</p>
-          <button className='bg-blue-500 text-white px-4 py-2 rounded'>قراءة المزيد</button>
+         <div className='p-4 text-end'>
+          <h3>اسم الكتاب </h3>
+          <p>وصف الكتاب</p>
+          <p>سعر الكتاب</p>
+          <div className='flex items-center justify-between '>
+            <button className='bg-[#408c7b] text-white px-4 py-2 rounded'>قراءة المزيد</button>
+          <p>تقيم الكتاب</p>
+        </div>
+          </div>
         </div></SwiperSlide>
-      <SwiperSlide>    <div className=' rounded-md border-1 boreder-[#b2ddd0]'>
+      <SwiperSlide>      <div className=' rounded-lg border-1 boreder-[rgb(64 140 123)] overflow-hidden'>
           <div className='relative overflow-hidden'>
 
-        <img src={img} alt='this book img' className=' border-b-1 rounded-md boreder-[#b2ddd0] hover:scale-105 duration-150 overflow-hidden peer '/>
-        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>llllllllllll</div>
+        <img src={img} alt='this book img' className=' border-b-1 boreder-[#b2ddd0] rounded-lg hover:scale-105 duration-150 overflow-hidden peer '/>
+        <div className='absolute w-full  left-0 right-0 bottom-0 bg-white p-6 translate-y-[100%] peer-hover:translate-y-0 duration-150'>
+          <div className='flex items-center justify-center gap-2'>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <AddCircleOutlineIcon />
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+<RemoveCircleOutlineIcon/>
+            </div>
+            <div className='bg-[#408c7b] text-white text-center p-2  rounded-full'>
+            <VisibilityIcon/>
+            </div>
+          </div>
+        </div>
         </div>
         
-          <h3 className='text-center'>عنوان الكتاب</h3>
-          <p className='text-center'>وصف الكتاب</p>
-          <button className='bg-blue-500 text-white px-4 py-2 rounded'>قراءة المزيد</button>
+         <div className='p-4 text-end'>
+          <h3>اسم الكتاب </h3>
+          <p>وصف الكتاب</p>
+          <p>سعر الكتاب</p>
+          <div className='flex items-center justify-between '>
+            <button className='bg-[#408c7b] text-white px-4 py-2 rounded'>قراءة المزيد</button>
+          <p>تقيم الكتاب</p>
+        </div>
+          </div>
         </div></SwiperSlide>
       
     </Swiper>
@@ -88,6 +161,8 @@ export const Slider = () => {
           <div className='basis-1/2 text-end max-md:basis-full max-md:mb-3.5'>
             <h2 className='text-4xl font-bold mb-5 border-l-3 border-[#f84f3b]'>اكتشف أفضل الكتب المميزة التي اخترناها لك</h2>
             <p>نوفر لك مجموعة مختارة بعناية من الكتب المميزة التي ستساعدك على التفوق في دراستك. هدفنا هو توفير وقتك من خلال تقديم أفضل الخيارات التي تناسب احتياجاتك الأكاديمية.</p>
+          
+           
         </div>
     </div>
     </>
